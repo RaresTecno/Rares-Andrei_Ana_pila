ef9d481d0c643bb2d075ab9513ddcff75f73d65b
RaresTecno <116293044+RaresTecno@users.noreply.github.com>
2023-02-21 16:36:37 +0100

    prueba 1

--- src/Rares_Pila/Pila.java
+++ src/Rares_Pila/Pila.java
@@ -10,7 +10,7 @@ public class Pila <E>{
     private Nodo <E> cabeza = null;
     
     public Pila(){
-        
+        //Esto es una prueba
     }
     public Pila(E [] array){ //Para crear una pila ya con objetos
         
